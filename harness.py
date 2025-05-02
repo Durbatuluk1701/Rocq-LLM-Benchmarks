275d6d625184664aa6981e28ae2cde2d0cebcc2d
BRichli <b748r023@ku.edu>
2025-05-02 13:23:06 -0500

    added timeout to harness.py

--- harness.py
+++ harness.py
@@ -1,2 +1,18 @@
 # This is where I want to write up the harness
 # I don't have code yet but it'll be fine
+
+
+'''
+# Timeout for python in linux use if needed 
+def handler(signum, frame):
+    raise Exception('TIMEOUT\n\n')
+
+
+signal.signal(signal.SIGALRM, handler)
+
+signal.alarm(totalTime)
+time = lap()
+try:
+
+except Exception as e:
+'''
