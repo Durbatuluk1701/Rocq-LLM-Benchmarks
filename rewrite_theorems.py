2b6e338111a3a5b372b65a62f2d6b1c8466cf39a
BRichli <b748r023@ku.edu>
2025-05-05 15:08:44 -0500

    adding in helper function stubs

--- rewrite_theorems.py
+++ rewrite_theorems.py
@@ -1,8 +1,21 @@
 """
 Placeholder
 """
-
+from random import randint
 
 def rewrite_theorems(file_contents: str) -> str:
     # TODO: Fill in
     return file_contents
+
+
+def rectify_proofs(file_contents: str) -> str:
+    """Rewrites all proofs to undo the rewrites to theorems, 
+    so that the original proofs continue to work as before"""
+    # TODO: Fill in
+    return file_contents
+
+def get_matching_identities(identities: list[str], theorem: str) -> dict[str, str]: 
+    """Using a list of identities and a theorem returns a dictionary with the 
+    snippet of the theorem that can be rewritten as the key and the updated snippet as value"""
+    return {}
+
